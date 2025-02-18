04e6f6578800f4d74a42e2337f1f968692ed66c0
河本峻輔 <s.kawamoto@kawamotoshuntasukunoMacBook-Pro.local>
2025-02-18 10:40:30 +0900

    Update reference ID generation to use 4-digit prefix and 9-digit random part

--- app/utils/reference-id.ts
+++ app/utils/reference-id.ts
@@ -1,16 +1,17 @@
 export function generateReferenceId(store: string): string {
-    const storePrefix =
-      {
-        "SPLASH'N'GO!前橋50号店": "1001",
-        "SPLASH'N'GO!伊勢崎韮塚店": "1002",
-        "SPLASH'N'GO!高崎棟高店": "1003",
-        "SPLASH'N'GO!足利緑町店": "1004",
-      }[store] || "1000"
-  
-    const randomPart = Math.floor(Math.random() * 1000000000000)
-      .toString()
-      .padStart(12, "0")
-  
-    return `${storePrefix}${randomPart}`
-  }
+  const storePrefix =
+    {
+      "SPLASH'N'GO!前橋50号店": "1001",
+      "SPLASH'N'GO!伊勢崎韮塚店": "1002",
+      "SPLASH'N'GO!高崎棟高店": "1003",
+      "SPLASH'N'GO!足利緑町店": "1004",
+    }[store] || "1000"
+
+  const randomPart = Math.floor(Math.random() * 1000000000)
+    .toString()
+    .padStart(9, "0")
+
+  return `${storePrefix}${randomPart}`
+}
+
   
